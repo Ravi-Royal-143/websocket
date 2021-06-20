9c4ac9651042a00df2f8feecca32e927d5ce7e6a
Ravikumar C S <raviroyalmech233@gmail.com>
2021-06-20 21:01:01 +0530

    inital commit

---
+++ app/index.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const cors = require('cors');
+const port = 3001;
+
+
+const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http, {
+    cors: {
+        origins: ['*']
+    }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message-broadcast', msg);
+    });
+});
+
+http.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
