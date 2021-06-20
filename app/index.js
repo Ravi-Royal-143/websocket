3ee1db3afb96fedfd38310dbebb40d3c3c8a47c6
Ravikumar C S <raviroyalmech233@gmail.com>
2021-06-20 21:36:45 +0530

    checking

--- app/index.js
+++ app/index.js
@@ -6,20 +6,27 @@ const port = 3001;
 
 const app = express();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http, {
-    cors: {
-        origins: ['*']
-    }
+// var io = require('socket.io')(http, {
+//     cors: {
+//         origins: ['*']
+//     }
+// });
+
+// app.use(cors());
+
+// io.on('connection', (socket) => {
+//     socket.on('message', (msg) => {
+//         socket.broadcast.emit('message-broadcast', msg);
+//     });
+// });
+
+app.get("/working", (req, res) => {
+    res.send("working")
 });
 
-app.use(cors());
-
-io.on('connection', (socket) => {
-    socket.on('message', (msg) => {
-        socket.broadcast.emit('message-broadcast', msg);
-    });
+app.get("/", (req, res) => {
+    res.send("okay")
 });
-
 
 http.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
