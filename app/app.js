975a7a4095a705ca9b345ac393e19023bd3868e9
Ravikumar C S <raviroyalmech233@gmail.com>
2021-07-08 19:01:06 +0530

    cors origin

--- app/app.js
+++ app/app.js
@@ -11,6 +11,19 @@ var io = require('socket.io')(http, {
     }
 });
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
+
 app.use(cors());
 app.use("/images", express.static(path.join("images")));
 
